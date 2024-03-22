bc4d89ea0090bfd3b6c4943eb3ec457c4fd34474
nwin19 <nandarwinbuma@gmail.com>
2024-03-23 03:34:36 +0630

    create sign in api route

--- api/controllars/auth.controllar.js
+++ api/controllars/auth.controllar.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,7 +11,32 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User created" });
   } catch (error) {
-    // next(errorHandler(550, "Error from the function signup")); 
+    // next(errorHandler(550, "Error from the function signup"));
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, "User not found")); //if user not found
+    }
+    const validPw = bcryptjs.compareSync(password, user.password); //compare the password
+    if (!validPw)
+      //if password is not valid
+      return next(errorHandler(401, "Password is not valid")); //401 is for unauthorized
+    const token = jwt.sign({ id: user._id }, process.env.jwt_SECRET);
+    const { password: pw, ...rest } = user._doc; //to not send the password to the client
+    res
+      .cookie("token", token, {
+        httpOnly: true, //to prevent cross site scripting
+        // expires: new Date(Date.now() + 24 * 60 * 60 * 1000), //to set the expiration date
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllars/auth.controllar.js";
+import { signin, signup } from "../controllars/auth.controllar.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
